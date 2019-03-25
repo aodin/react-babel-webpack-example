554dc39640d653c54b1b1fb488a690aeb0916250
Aaron O. Ellis <aaronoellis@gmail.com>
2019-03-25 17:00:48 -0600

    Compile CSS or SCSS

--- webpack.common.js
+++ webpack.common.js
@@ -55,7 +55,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(scss)$/,
+        test: /\.(s?)css$/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
